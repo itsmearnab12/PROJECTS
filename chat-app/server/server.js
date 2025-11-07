d15f7fea26f05bde1bc5aae9da358ab1bab5c459
Arnab <arnabchoudhury2005@gmail.com>
2025-11-07 12:13:11 +0530

    Stated working on backend

--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -2,20 +2,16 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
-import { connectDB } from "./lib/db";
 
-//Creating Express app using HTTP server
+//Creating Express app and HTTP server
 const app = express();
 const server = http.createServer(app)
 
-// setting Middleware
+//Settingup Middleware
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
-app.use("/api/status", (req, res)=> res.send("server is live"));
-
-//connect to MongoDB
-await connectDB();
+app.use("/api/status", (req, res)=> res.send("Server is live"));
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("server is running on PORT:" + PORT))
+server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
