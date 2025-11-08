60162a8653e8fff13370c796b28daa6635583f09
Arnab <arnabchoudhury2005@gmail.com>
2025-11-08 09:46:38 +0530

    Work in progress

---
+++ chat-app/server/controllers/userController.js
@@ -0,0 +1,37 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+
+//Signup a new user
+export const Signup = async (req, res)=> {
+    const { fullName, email, password, bio } = req.body;
+
+    try {
+        if(!fullName || !email || !password || !bio){
+            return res.json({success: false, message: "Missing Details"})
+        }
+        const user = await User.findOne({email});
+
+        if (user) {
+            return res.json({success: false, message: "Account already exists"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            fullName, email, password: hashedPassword, bio
+        });
+
+        const token = generateToken(newUser._id)
+
+        res.json({success: true, userData: newUser, token, message: "Account created successfully"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//Contoller to login a user
+//2:44:25
