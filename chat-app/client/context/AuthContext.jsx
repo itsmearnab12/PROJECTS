df428b67d7e1a03b9eeebef99b4c94bc2bbbc52d
Arnab <arnabchoudhury2005@gmail.com>
2025-11-14 09:57:49 +0530

    working in connection of backend and frontend

---
+++ chat-app/client/context/AuthContext.jsx
@@ -0,0 +1,72 @@
+import { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+import toast from "react-hot-toast";
+import { io } from "socket.io.client";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = backendUrl;
+
+export const AuthContext = createContext();
+
+
+export const AuthProvider = ({ children }) => {
+
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const [authUser, setAuthUser] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState([]);
+    const [socket, setSocket] = useState(null);
+
+    //Check if user is authenticated and if so, set the user data and connect the socket
+    const checkAuth = async () => {
+        try {
+            const { data } = await axios.get("/api/auth/check");
+            if (data.success) {
+                setAuthUser(data.user)
+                connectSocket(data.user)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    //Login function to handle user authentication and socket connection
+    //4:06:46
+
+    //Connect socket function to handle socket connection and online users updates
+    const connectSocket = (userData)=>{
+        if(!userData || socket?.connected) return;
+        const newSocket = io(backendUrl, {
+            query: {
+                userId: userData._id,
+            }
+        });
+        newSocket.connect();
+        setSocket(newSocket);
+
+        newSocket.on("getOnlineUsers", (userIds)=>{
+            setOnlineUsers(userIds);
+        })
+    }
+
+
+    useEffect(()=>{
+        if (token) {
+            axios.defaults.headers.common["token"] = token;
+        }
+        checkAuth();
+    }, [])
+
+    const value = {
+        axios,
+        authUser,
+        onlineUsers,
+        socket
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
