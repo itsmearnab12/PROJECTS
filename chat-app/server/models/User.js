75e6e2a631cfd550e529eecb02c92365f5ee4ca6
Arnab <arnabchoudhury2005@gmail.com>
2025-11-18 23:23:43 +0530

    fixed one bug

--- chat-app/server/models/User.js
+++ chat-app/server/models/User.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     password: {type: String, required: true, minlength: 6},
     profilePic: {type: String, default: ""},
     bio: {type: String},
-}, {tiimestamps: true})
+}, {timestamps: true})
 
 const User = mongoose.model("User", userSchema);
 
