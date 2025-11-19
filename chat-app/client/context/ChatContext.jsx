ba522e9494562ad9a50baaad45cd9bb05f02913f
Arnab <arnabchoudhury2005@gmail.com>
2025-11-19 19:35:50 +0530

    One another bug

--- chat-app/client/context/ChatContext.jsx
+++ chat-app/client/context/ChatContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
 import toast from "react-hot-toast";
 
@@ -54,10 +54,34 @@ export const ChatProvider = ({ children })=>{
     }
 
     //function to subscribe to messages for selected user
-    // 4:52:00
+    const subscribeToMessages = async () =>{
+        if (!socket)  return;
+
+        socket.on("newMessage", (newMessage)=>{
+            if (selectedUser && newMessage.senderId === selectedUser._id) {
+                newMessage.seen = true;
+                setMessages((prevMessages)=> [...prevMessages, newMessage])
+                axios.put(`/api/messages/mark/${newMessage._id}`);
+            }else{
+                setUnseenMessages((prevUnseenMessages)=>({
+                    ...prevUnseenMessages, [newMessage.senderId] : prevUnseenMessages[newMessage.senderId] ? prevUnseenMessages[newMessage.senderId] + 1 : 1
+                }))
+            }
+        })
+    }
+
+    //function to unsubscribe from messages
+    const unsubscribeFromMessages = ()=>{
+        if (socket) socket.off("newMessage");
+    }
+
+    useEffect(()=>{
+        subscribeToMessages();
+        return ()=> unsubscribeFromMessages();
+    }, [socket, selectedUser])
 
     const value = {
-
+        messages, users, selectedUser, getUsers, setMessages, sendMessage, setSelectedUser, unseenMessages, setUnseenMessages
     }
 
     return (
