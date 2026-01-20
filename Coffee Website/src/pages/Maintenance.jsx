f27c526b9cd44f554e2519002698752cfb53005a
Arnab <arnabchoudhury2005@gmail.com>
2026-01-20 17:39:40 +0530

    Learning and Building

--- Coffee Website/src/pages/Maintenance.jsx
+++ Coffee Website/src/pages/Maintenance.jsx
@@ -4,12 +4,12 @@ import "./maintenance.css";
 const Maintenance = () => {
   return (
     <div className="maintenance">
-      <h1>🚧 We’ll Be Back Soon</h1>
+      <h1>🚧 Website Under Maintenance</h1>
       <p>
         Our website is currently under maintenance.<br />
         We’re working hard to improve your experience.
       </p>
-      <span>Thank you for your patience 🙏</span>
+      <span>For Queries: support@oakandroast.com</span>
     </div>
   );
 };
