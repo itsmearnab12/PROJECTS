2981a834b49cf9fc5fa93283f05326e7a2a69a9a
Arnab <arnabchoudhury2005@gmail.com>
2026-03-25 12:21:32 +0530

    Learning and Building

--- Finance Tracker Application/Frontend/Finance Tracker Application/src/pages/LoginPage.jsx
+++ Finance Tracker Application/Frontend/Finance Tracker Application/src/pages/LoginPage.jsx
@@ -2,42 +2,45 @@ import React, { useState } from 'react'
 import { FaUser } from "react-icons/fa";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import "./Loginpage.css"
 const LoginPage = () => {
 
   const [state, setState] = useState('Sign up');
 
   return (
-    <div>
-      <div>
-        <h2>{state === 'Sign up' ? 'Welcome to Fintech' : 'Create Your Account'}</h2>
-        <h2>{state === 'Sign up' ? 'Welcome to Fintech' : 'Login To Your Account'}</h2>
-      </div>
-      <form>
-        {state === 'Sign up' && (
-          <div>
-            <FaUser />
-            <input type="text" placeholder='Full Name' required />
+    <div className='form'>
+      <div className='form-box'>
+        <div>
+          <h2>{state === 'Sign up' ? 'Welcome to Fintech' : 'Create Your Account'}</h2>
+          <h2>{state === 'Sign up' ? 'Welcome to Fintech' : 'Login To Your Account'}</h2>
+        </div>
+        <form>
+          {state === 'Sign up' && (
+            <div className='fromdetails'>
+              <FaUser />
+              <input type="text" placeholder='Full Name' required />
+            </div>
+          )}
+          <div className='fromdetails'>
+            <MdOutlineMailOutline />
+            <input type="Email" placeholder='Email Id' required />
           </div>
+          <div className='fromdetails'>
+            <RiLockPasswordLine />
+            <input type="Password" placeholder='Password' required />
+          </div>
+          <button>{state}</button>
+        </form>
+        {state === 'Sign up' ? (
+          <p>Already have an account?{' '}
+            <span onClick={() => setState('Login')}>Login here</span>
+          </p>
+        ) : (
+          <p>Don't have an account?{' '}
+            <span onClick={() => setState('Sign up')}>Signup here</span>
+          </p>
         )}
-        <div>
-          <MdOutlineMailOutline />
-          <input type="Email" placeholder='Email Id' required />
-        </div>
-        <div>
-          <RiLockPasswordLine />
-          <input type="Password" placeholder='Password' required />
-        </div>
-        <button>{state}</button>
-      </form>
-      {state === 'Sign up' ? (
-        <p>Already have an account?{' '}
-          <span onClick={()=> setState('Login')}>Login here</span>
-        </p>
-      ) : (
-        <p>Don't have an account?{' '}
-          <span onClick={()=> setState('Sign up')}>Signup here</span>
-        </p>
-      )}
+      </div>
     </div>
   )
 }
