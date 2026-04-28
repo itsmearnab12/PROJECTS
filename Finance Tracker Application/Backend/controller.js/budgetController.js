8253721f5f84f75dd26dd8b29da3d465f212cbb1
Arnab <arnabchoudhury2005@gmail.com>
2026-04-28 23:33:05 +0530

    Working on backend for Budget section of finance tracker

---
+++ Finance Tracker Application/Backend/controller.js/budgetController.js
@@ -0,0 +1,71 @@
+import { Budget } from "../models.js/Budget.js";
+import { Transaction } from "../models.js/Transaction.js";
+
+//Contoller function for Adding Budget
+export const addBudget = async (req, res) => {
+  try {
+    const { title, limit } = req.body;
+
+    if (!title || !limit) {
+      return res.json({
+        success: false,
+        message: "All fields required",
+      });
+    }
+
+    const budget = await Budget.create({
+      userId: req.userId,
+      title,
+      limit,
+    });
+
+    res.json({
+      success: true,
+      budget,
+    });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Contoller function for getting budget
+export const getBudgets = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    //Getting all Budget
+    const budgets = await Budget.find({ userId });
+
+    //Getting all expense transaction
+    const transactions = await Transaction.find({
+      userId,
+      type: "expense",
+    });
+
+    //Attach spent + remaning
+    const result = budgets.map((budget) => {
+      const spent = transactions
+        .filter((t) => t.category === budget.title)
+        .reduce((sum, t) => sum + t.amount, 0);
+
+      const remaning = budget.limit - spent;
+
+      return {
+        ...budget._doc,
+        spent,
+        remaning,
+        percent: budget.limit > 0 ? (spent / budget.limit) * 100 : 0,
+      };
+    });
+
+    res.json({
+      success: true,
+      budgets: result,
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
---
+++ Finance Tracker Application/Backend/models.js/Budget.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const budgetSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      red: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    limit: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Budget = mongoose.model("Budget", budgetSchema);
